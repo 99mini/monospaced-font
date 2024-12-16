216de1faa4a541e22cbefa0bb7f102b9b204e077
mimi99 <0mini9939@gmail.com>
2024-12-17 02:35:19 +0900

    chore: update ESLint configuration and dependencies, refactor App component, and adjust TypeScript settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,41 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 import prettier from 'eslint-plugin-prettier';
+import parser from '@typescript-eslint/parser';
+import globals from 'globals';
 
-export default tseslint.config(
-  { ignores: ['dist', '.yarn', 'node_modules'] },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      'plugin:prettier/recommended', // Prettier 연동 추가
-    ],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default {
+  files: ['**/*.{ts,tsx}'], // ESLint가 적용될 파일 패턴
+  ignores: ['dist', '.yarn/**', 'node_modules', '.pnp'], // 무시할 경로
+  languageOptions: {
+    ecmaVersion: 'latest', // 최신 ECMAScript 기능 사용
+    sourceType: 'module', // ECMAScript 모듈
+    parser,
+    globals: {
+      ...globals.browser, // 브라우저 전역 객체
+      ...globals.node, // Node.js 전역 객체
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier, // Prettier 플러그인 추가
+  },
+  plugins: {
+    react, // React 플러그인
+    'react-hooks': reactHooks,
+    prettier, // Prettier 플러그인
+    '@typescript-eslint': tsPlugin, // TypeScript ESLint 플러그인
+  },
+  rules: {
+    ...js.configs.recommended.rules, // JavaScript 추천 규칙
+    ...react.configs.recommended.rules, // React 추천 규칙
+    ...reactHooks.configs.recommended.rules, // React Hooks 추천 규칙
+    ...tsPlugin.configs.recommended.rules, // TypeScript 추천 규칙
+    'prettier/prettier': 'error', // Prettier 규칙
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect', // React 버전 자동 감지
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'prettier/prettier': 'error', // Prettier 규칙 활성화
-    },
-  }
-);
+  },
+};
